00bd87d177ccf2ecdad8d8842d2d432266761176
LemonHaze420 <35783139+LemonHaze420@users.noreply.github.com>
2018-12-28 17:36:24 +0000

    Initial commit

---
+++ model2obj/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShenmueDKSharp.Files.Models;
+
+namespace model2obj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count<string>() < 3 || args[0].Contains("-h") || args[0].Contains("--help") || args[0].Contains("/?"))
+            {
+                Console.WriteLine("Correct usage:\n\tmodel2obj [--mt5|--mt7] <source> <destination>");
+                return;
+            }
+
+            bool MT5convert = false, MT7convert = false;
+
+            string modelSource;
+            string modelDestination;
+
+            modelSource         = args[1];
+            modelDestination    = args[2];
+            Console.WriteLine("Source: {0}\nDestination: {1}", modelSource, modelDestination);
+
+            if((args[0].Contains("--mt5") || args[0].Contains("-mt5")))
+               MT5convert = true;
+            if ((args[0].Contains("--mt7") || args[0].Contains("-mt7")))
+               MT7convert = true;
+            
+            if(MT5convert)
+            {
+                MT5 tmpMT5 = new MT5(modelSource);
+                OBJ tmpOBJ = new OBJ(tmpMT5);
+
+                tmpOBJ.Write(modelDestination);
+            }
+            else if (MT7convert)
+            {
+                MT7 tmpMT7 = new MT7(modelSource);
+                OBJ tmpOBJ = new OBJ(tmpMT7);
+
+                tmpOBJ.Write(modelDestination);
+            }
+        }
+    }
+}
---
+++ model2obj/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("model2obj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("model2obj")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f2a79431-b634-47f7-b3bc-6bb440a98a8a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
