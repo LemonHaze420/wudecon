8e075a4e273d6fdbbdc15bfdfa8af2a7ad46d428
LemonHaze420 <35783139+LemonHaze420@users.noreply.github.com>
2018-12-28 18:48:41 +0000

    Added PKF, PKS and TAC extraction

--- model2obj/Program.cs
+++ model2obj/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using ShenmueDKSharp;
 using ShenmueDKSharp.Files.Models;
+using ShenmueDKSharp.Files.Containers;
+using ShenmueDKSharp.Files;
 
 namespace model2obj
 {
@@ -22,12 +24,33 @@ namespace model2obj
             OBJ obj = new OBJ(mt5);
             obj.Write(objFilepath);
         }
+        static void ExtractPKF(string pkfFilepath, string folder)
+        {
+            PKF pkf = new PKF(pkfFilepath);
+            pkf.Unpack(folder);
+        }
+        static void ExtractPKS(string pksFilepath, string folder)
+        {
+            PKS pks = new PKS(pksFilepath);
+            pks.Unpack(folder);
+        }
+        static void ExtractSPR(string sprFilepath, string folder)
+        {
+            SPR spr = new SPR(sprFilepath);
+            spr.Unpack(folder);
+        }
+        static void ExtractTAC(string tadFilepath, string tacFilepath, string folder)
+        {
+            TAD tad = new TAD();
+            tad.FileName = tadFilepath;
+            tad.Unpack(tacFilepath, folder);
+        }
 
         static void Main(string[] args)
         {
             if (args.Count<string>() < 3 || args[0].Contains("-h") || args[0].Contains("--help") || args[0].Contains("/?"))
             {
-                Console.WriteLine("Correct usage:\n\tmodel2obj [--mt5|--mt7] <source> <destination>");
+                Console.WriteLine("Correct usage:\n\tmodel2obj [--mt5|--mt7|--pkf|--pks|--tac] <source> <destination>");
                 return;
             }
 
@@ -35,6 +58,12 @@ namespace model2obj
                 ExportMT5(args[1], args[2]);
             if ((args[0].Contains("--mt7") || args[0].Contains("-mt7")))
                 ExportMT7(args[1], args[2]);
+            if ((args[0].Contains("--pkf") || args[0].Contains("-pkf")))
+                ExtractPKF(args[1], args[2]);
+            if ((args[0].Contains("--pks") || args[0].Contains("-pks")))
+                ExtractPKS(args[1], args[2]);
+            if ((args[0].Contains("--tac") || args[0].Contains("-tac")))
+                ExtractTAC(args[1], args[2], args[3]);
         }
     }
 }
