717ddc176f98a72790f51975568effc13984c2d1
guizhang <lemonhazesdankmemes@gmail.com>
2018-12-29 17:17:09 +0000

    * Added batch conversion when a directory is passed to path argument in all funcs
    * Removed pointless batch flag

--- wudecon/Program.cs
+++ wudecon/Program.cs
@@ -14,168 +14,267 @@ namespace wudecon
 {
     class Program
     {
-        static void ExportMT7(string mt7Filepath, string objFilepath, bool batch = false)
+        static void ExportMT7(string path, string objFilepath)
         {
-            if (!batch)
+            if (!File.Exists(path))
             {
-                MT7 mt7 = new MT7(mt7Filepath);
+                Console.WriteLine("{0} does not exist as a file, falling back to batch mode.", path);
+
+                if (Directory.Exists(path))
+                {
+                    var ext = new List<string> { ".mt7", ".MT7" };
+                    var myFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+
+                    foreach (string file in myFiles)
+                    {
+                        string dest = file;
+                        dest += ".OBJ";
+
+                        ExportMT7(file, dest);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} does not exist. Cancelling operation.", path);
+                    return;
+                }
+            }
+            else
+            {
+                MT7 mt7 = new MT7(path);
                 OBJ obj = new OBJ(mt7);
 
                 obj.Write(objFilepath);
             }
-            else
+
+            return;
+        }
+        static void ExportMT5(string path, string objFilepath)
+        {
+            if (!File.Exists(path))
             {
-                var ext = new List<string> { ".mt7", ".MT7" };
-                var myFiles = Directory.GetFiles(mt7Filepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+                Console.WriteLine("{0} does not exist as a file, falling back to batch mode.", path);
 
-                foreach (string file in myFiles)
+                if (Directory.Exists(path))
                 {
-                    string dest = file;
-                    dest += ".OBJ";
+                    var ext = new List<string> { ".mt5", ".MT5" };
+                    var myFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
 
-                    ExportMT7(file, dest);
+                    foreach (string file in myFiles)
+                    {
+                        string dest = file;
+                        dest += ".OBJ";
+
+                        ExportMT5(file, dest);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} does not exist. Cancelling operation.", path);
+                    return;
                 }
             }
-        }
-        static void ExportMT5(string mt5Filepath, string objFilepath, bool batch = false)
-        {
-            if (!batch)
+            else
             {
-                MT5 mt5 = new MT5(mt5Filepath);
+                MT5 mt5 = new MT5(path);
                 OBJ obj = new OBJ(mt5);
+
                 obj.Write(objFilepath);
             }
-            else
+            return;
+        }
+        static void ExtractPKF(string path, string folder)
+        {
+            if (!File.Exists(path))
             {
-                var ext = new List<string> { ".mt5", ".MT5" };
-                var myFiles = Directory.GetFiles(mt5Filepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+                Console.WriteLine("{0} does not exist as a file, falling back to batch mode.", path);
 
-                foreach (string file in myFiles)                {
-                    string dest = file;
-                    dest += ".OBJ";
+                if (Directory.Exists(path))
+                {
+                    var ext = new List<string> { ".pkf", ".PKF" };
+                    var myFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
 
-                    ExportMT5(file, dest);
+                    foreach (string file in myFiles)
+                    {
+                        PKF pkf = new PKF(file);
+                        pkf.Unpack(folder);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} does not exist. Cancelling operation.", path);
+                    return;
                 }
             }
-        }
-        static void ExtractPKF(string pkfFilepath, string folder, bool batch = false)
-        {
-            if (!batch)
+            else
             {
-                PKF pkf = new PKF(pkfFilepath);
+                PKF pkf = new PKF(path);
                 pkf.Unpack(folder);
             }
-            else
+        }
+        static void ExtractPKS(string pksFilepath, string folder)
+        {
+            if (!File.Exists(pksFilepath))
             {
-                var ext = new List<string> { ".pkf", ".PKF" };
-                var myFiles = Directory.GetFiles(pkfFilepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+                Console.WriteLine("{0} does not exist as a file, falling back to batch mode.", pksFilepath);
 
-                foreach (string file in myFiles)
+                if(Directory.Exists(pksFilepath))
                 {
-                    PKF pkf = new PKF(file);
-                    pkf.Unpack(folder);
+                    var ext = new List<string> { ".pks", ".PKS" };
+                    var myFiles = Directory.GetFiles(pksFilepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+
+                    foreach (string file in myFiles)
+                    {
+                        PKS pks = new PKS(file);
+                        pks.Unpack(folder);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} does not exist. Cancelling operation.", pksFilepath);
+                    return;
                 }
             }
-        }
-        static void ExtractPKS(string pksFilepath, string folder, bool batch = false)
-        {
-            if (!batch)
+            else
             {
                 PKS pks = new PKS(pksFilepath);
                 pks.Unpack(folder);
+                return;
+            }
+        }
+        static void ExtractSPR(string path, string folder)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("{0} does not exist as a file, falling back to batch mode.", path);
+
+                if (Directory.Exists(path))
+                {
+                    var ext = new List<string> { ".spr", ".SPR" };
+                    var myFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+
+                    foreach (string file in myFiles)
+                    {
+                        SPR spr = new SPR(file);
+
+                        spr.Unpack(folder);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} does not exist. Cancelling operation.", path);
+                    return;
+                }
             }
             else
             {
-                var ext = new List<string> { ".pks", ".PKS" };
-                var myFiles = Directory.GetFiles(pksFilepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+                SPR spr = new SPR(path);
 
-                foreach (string file in myFiles)
-                {
-                    PKS pks = new PKS(file);
-                    pks.Unpack(folder);
-                }
+                spr.Unpack(folder);    
             }
+
+            return;
         }
-        static void ExtractSPR(string sprFilepath, string folder, bool batch = false)
+        static void ExtractIPAC(string path, string folder)
         {
-            if (!batch)
+            if (!File.Exists(path))
             {
-                SPR spr = new SPR(sprFilepath);
-                spr.Unpack(folder);
+                Console.WriteLine("{0} does not exist as a file, falling back to batch mode.", path);
+
+                if (Directory.Exists(path))
+                {
+                    var ext = new List<string> { ".ipac", ".IPAC" };
+                    var myFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+
+                    foreach (string file in myFiles)
+                    {
+                        IPAC ipac = new IPAC(file);
+                        ipac.Unpack(folder);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} does not exist. Cancelling operation.", path);
+                    return;
+                }
             }
             else
             {
-                var ext = new List<string> { ".spr", ".SPR" };
-                var myFiles = Directory.GetFiles(sprFilepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
-
-                foreach (string file in myFiles)
-                {
-                    SPR spr = new SPR(file);
-                    spr.Unpack(folder);
-                }
-            }
-        }
-        static void ExtractIPAC(string ipacFilepath, string folder, bool batch = false)
-        {
-            if (!batch)
-            {
-                IPAC ipac = new IPAC(ipacFilepath);
+                IPAC ipac = new IPAC(path);
                 ipac.Unpack(folder);
             }
-            else
+            return;
+        }
+        static void ExtractGZ(string path, string folder)
+        {
+            if (!File.Exists(path))
             {
-                var ext = new List<string> { ".ipac", ".IPAC" };
-                var myFiles = Directory.GetFiles(ipacFilepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+                Console.WriteLine("{0} does not exist as a file, falling back to batch mode.", path);
 
-                foreach (string file in myFiles)
+                if (Directory.Exists(path))
                 {
-                    IPAC ipac = new IPAC(file);
-                    ipac.Unpack(folder);
+                    var ext = new List<string> { ".gz", ".GZ" };
+                    var myFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+
+                    foreach (string file in myFiles)
+                    {
+                        GZ gz = new GZ(file);
+
+                        gz.Unpack(folder);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} does not exist. Cancelling operation.", path);
+                    return;
                 }
             }
-        }
-        static void ExtractGZ(string gzFilepath, string folder, bool batch = false)
-        {
-            if (!batch)
+            else
             {
-                GZ gz = new GZ(gzFilepath);
+                GZ gz = new GZ(path);
+
                 gz.Unpack(folder);
             }
-            else
+            return;
+        }
+        static void ExtractAFS(string path, string folder)
+        {
+            if (!File.Exists(path))
             {
-                var ext = new List<string> { ".gz", ".GZ" };
-                var myFiles = Directory.GetFiles(gzFilepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+                Console.WriteLine("{0} does not exist as a file, falling back to batch mode.", path);
 
-                foreach (string file in myFiles)
+                if (Directory.Exists(path))
                 {
-                    GZ gz = new GZ(file);
-                    gz.Unpack(folder);
+                    var ext = new List<string> { ".afs", ".AFS" };
+                    var myFiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
+
+                    foreach (string file in myFiles)
+                    {
+                        AFS afs = new AFS(file);
+
+                        afs.Unpack(folder);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("{0} does not exist. Cancelling operation.", path);
+                    return;
                 }
             }
-        }
-        static void ExtractAFS(string afsFilepath, string folder, bool batch = false)
-        {
-            if (!batch)
+            else
             {
-                AFS afs = new AFS(afsFilepath);
+                AFS afs = new AFS(path);
+
                 afs.Unpack(folder);
             }
-            else
-            {
-                var ext = new List<string> { ".afs", ".AFS" };
-                var myFiles = Directory.GetFiles(afsFilepath, "*.*", SearchOption.AllDirectories).Where(s => ext.Contains(Path.GetExtension(s)));
 
-                foreach (string file in myFiles)
-                {
-                    AFS afs = new AFS(file);
-                    afs.Unpack(folder);
-                }
-            }
+            return;
         }
         static void ExtractTAC(string tadFilepath, string tacFilepath, string folder)
         {
             TAD tad = new TAD(tadFilepath);
             TAC tac = new TAC(tad);
+
             tac.Unpack(true, false, folder);
         }
 
@@ -183,12 +282,11 @@ namespace wudecon
         {
             Console.WriteLine("\twudecon <mode> <input> <output>");
             Console.WriteLine("\twudecon --mt5 <mt5 file> <obj file>");
+            Console.WriteLine("\twudecon --mt5 <dir with mt5's> <output dir>");
             Console.WriteLine("\twudecon --mt7 <mt7 file> <obj file>");
             Console.WriteLine("\twudecon [--pkf|--pks|--spr|--ipac|--gz|--afs] <source file> <output dir>");
             Console.WriteLine("\twudecon --tac <tad file> <tac file> <output dir>");
-            Console.WriteLine("\twudecon --batch-mt5 <mt5 dir> <obj output dir>");
 
-            Console.WriteLine("\nBatch flags:\n\t--batch-mt5\n\t--batch-mt7\n\t--batch-pkf\n\t--batch-pks\n\t--batch-spr\n\t--batch-ipac\n\t--batch-gz\n\t--batch-afs");
         }
 
         static void Main(string[] args)
@@ -206,18 +304,10 @@ namespace wudecon
             {
                 ExportMT5(args[1], args[2]);
             }
-            else if ((args[0].Contains("--batch-mt5") || args[0].Contains("-bmt5")))
-            {
-                ExportMT5(args[1], args[2], true);
-            }
 
             if ((args[0].Contains("--mt7") || args[0].Contains("-mt7")))
             {
                 ExportMT7(args[1], args[2]);
-            }
-            else if ((args[0].Contains("--batch-mt7") || args[0].Contains("-bmt7")))
-            {
-                ExportMT7(args[1], args[2], true);
             }
 
             // Container conversions
@@ -225,55 +315,29 @@ namespace wudecon
             {
                 ExtractPKF(args[1], args[2]);
             }
-            else if ((args[0].Contains("--batch-pkf") || args[0].Contains("-bpkf")))
-            {
-                ExtractPKF(args[1], args[2], true);
-            }
             if ((args[0].Contains("--pks") || args[0].Contains("-pks")))
             {
                 ExtractPKS(args[1], args[2]);
-            }
-            else if ((args[0].Contains("--batch-pks") || args[0].Contains("-bpks")))
-            {
-                ExtractPKS(args[1], args[2], true);
             }
 
             if ((args[0].Contains("--spr") || args[0].Contains("-spr")))
             {
                 ExtractSPR(args[1], args[2]);
             }
-            else if ((args[0].Contains("--batch-spr") || args[0].Contains("-bspr")))
-            {
-                ExtractSPR(args[1], args[2], true);
-            }
-
 
             if ((args[0].Contains("--ipac") || args[0].Contains("-ipac")))
             {
                 ExtractIPAC(args[1], args[2]);
-            }
-            else if ((args[0].Contains("--batch-ipac") || args[0].Contains("-ipac")))
-            {
-                ExtractIPAC(args[1], args[2], true);
             }
 
             if ((args[0].Contains("--gz") || args[0].Contains("-gz")))
             {
                 ExtractGZ(args[1], args[2]);
             }
-            else if ((args[0].Contains("--batch-gz") || args[0].Contains("-bgz")))
-            {
-                ExtractGZ(args[1], args[2], true);
-            }
 
             if ((args[0].Contains("--afs") || args[0].Contains("-afs")))
             {
                 ExtractAFS(args[1], args[2]);
-            }
-            else if ((args[0].Contains("--batch-afs") || args[0].Contains("-bafs")))
-            {
-                ExtractAFS(args[1], args[2], true);
-
             }
 
             if ((args[0].Contains("--tac") || args[0].Contains("-tac")))
